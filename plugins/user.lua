0cfdd4138aafb9b3f61c6d0ff75f29a7e96355b0
Rishikesh Betigeri <rishikeshb01@gmail.com>
2023-09-21 23:53:19 +0530

    mychange

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,48 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  -- {
+   --{
+   --"ray-x/go.nvim",
+--    dependencies = {  -- optional packages
+--      "ray-x/guihua.lua",
+--      "neovim/nvim-lspconfig",
+--      "nvim-treesitter/nvim-treesitter",
+--      "theHamsta/nvim-dap-virtual-text",
+--    },
+--    config = function()
+--      require("go").setup()
+--    end,
+--    event = {"CmdlineEnter"},
+--   ft = {"go", 'gomod'},
+--    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+-- },
+--
+  -- {
+  --   "olexsmir/gopher.nvim",
+  --   ft = "go",
+  --   config = function(_, opts)
+  --     require("gopher").setup(opts)
+  --     require("core.utils").load_mappings("gopher")
+  --   end,
+  --   build = function()
+  --     vim.cmd [[silent! GoInstallDeps]]
+  --   end,
+  -- },
+  --
+  --
+
+-- {
+--     "iamcco/markdown-preview.nvim",
+--     lazy = false,
+--     config = function()
+--       vim.fn["mkdp#util#install"]()
+--     end,
+-- },
+--
+ {
+  "theHamsta/nvim-dap-virtual-text",
+  ft = "go",
+ },
 }
